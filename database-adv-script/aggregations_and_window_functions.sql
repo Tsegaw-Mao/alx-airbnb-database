60d5cc92574376954803fccda96533cd4a1bfc07
Tsegaw-Mao <tsegawjohnj@gmail.com>
2025-07-05 14:22:38 +0300

    agrregation and window functions

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,3 +1,4 @@
+-- COUNT function and GROUP BY  --
 SELECT
     Users.user_id,
     Users.first_name,
@@ -7,20 +8,41 @@ SELECT
     COUNT(Bookings.booking_id) AS total_bookings
 FROM
     Users
-LEFT JOIN
-    Bookings ON Users.user_id = Bookings.user_id
+    LEFT JOIN Bookings ON Users.user_id = Bookings.user_id
 GROUP BY
-    Users.user_id, Users.first_name, Users.last_name, Users.email, Users.phone_number
+    Users.user_id,
+    Users.first_name,
+    Users.last_name,
+    Users.email,
+    Users.phone_number
 
+-- Window functions: RANK and ROW_NUMBER  --
 SELECT
-    Properties.property_id,
-    Properties.name,
-    Properties.location,
-    COUNT(Bookings.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(Bookings.booking_id) DESC) AS Properties_rank
+    ROW_NUMBER() OVER (
+        ORDER BY
+            total_bookings DESC
+    ) AS Property_Num, -- 1, 2, 3 for row numbering of properties
+    property_id, -- property ID
+    name, -- property name
+    location, -- property location
+    total_bookings, -- total bookings for the property
+    RANK() OVER (
+        ORDER BY
+            total_bookings DESC
+    ) AS Properties_rank -- rank based on total bookings
 FROM
-    Properties
-LEFT JOIN
-    Bookings ON Properties.property_id = Bookings.property_id
-GROUP BY
-    Properties.property_id, Properties.name, Properties.location
+    (
+        -- Inner query: total bookings per property
+        SELECT
+            Properties.property_id, -- property ID
+            Properties.name, -- property name       
+            Properties.location, -- property location   
+            COUNT(Bookings.booking_id) AS total_bookings
+        FROM
+            Properties
+            LEFT JOIN Bookings ON Properties.property_id = Bookings.property_id
+        GROUP BY
+            Properties.property_id,
+            Properties.name,
+            Properties.location
+    ) AS ranked_properties;
